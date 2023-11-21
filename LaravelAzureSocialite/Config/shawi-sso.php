fe7155c0e441f58677de61b4e55c38a0ae38819c
Patrice Guillemette <pguillemette@shawinigan.ca>
2023-11-21 13:13:47 -0500

    Use Flowbite for the welcome view and added preview

--- LaravelAzureSocialite/Config/shawi-sso.php
+++ LaravelAzureSocialite/Config/shawi-sso.php
@@ -6,5 +6,5 @@ return [
      * The name of the route to be redirected to after succesful login.
      */
     'dashboardRouteName' => 'dashboard',
-
+    'privacyPolicyLink' => 'https://www.shawinigan.ca/mentions-legales/'
 ];
